b4e55680e120cab9b776edfab69c1f8090634c1a
WavyPeng <w1a9v9y3@gmail.com>
2020-08-27 00:21:50 +0800

    Duck基类，定义基础属性、方法

---
+++ Strategy/src/main/java/head/first/design/pattern/strategy/Duck.java
@@ -0,0 +1,28 @@
+package head.first.design.pattern.strategy;
+
+/**
+ * @author: WavyPeng
+ * @date: 2020/08/27
+ */
+public abstract class Duck {
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public abstract void display();
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
+}
