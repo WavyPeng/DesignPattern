8c9f065591ac893bfbc54b623519c05f2d18842a
WavyPeng <w1a9v9y3@gmail.com>
2020-08-27 23:41:22 +0800

    实现叫声行为接口

---
+++ Strategy/src/main/java/head/first/design/pattern/strategy/MuteQuack.java
@@ -0,0 +1,11 @@
+package head.first.design.pattern.strategy;
+
+/**
+ * @author: WavyPeng
+ * @date: 2020/8/27
+ */
+public class MuteQuack implements QuackBehavior{
+    public void quack() {
+        System.out.println("silence");
+    }
+}
---
+++ Strategy/src/main/java/head/first/design/pattern/strategy/Quack.java
@@ -0,0 +1,11 @@
+package head.first.design.pattern.strategy;
+
+/**
+ * @author: WavyPeng
+ * @date: 2020/8/27
+ */
+public class Quack implements QuackBehavior{
+    public void quack() {
+        System.out.println("quack");
+    }
+}
