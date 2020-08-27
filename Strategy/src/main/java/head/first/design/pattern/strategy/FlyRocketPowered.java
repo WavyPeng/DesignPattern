35c303cafa963415131b4d303086d57b5ad7c6d0
WavyPeng <w1a9v9y3@gmail.com>
2020-08-27 23:36:57 +0800

    实现飞行行为接口

---
+++ Strategy/src/main/java/head/first/design/pattern/strategy/FlyRocketPowered.java
@@ -0,0 +1,11 @@
+package head.first.design.pattern.strategy;
+
+/**
+ * @author: WavyPeng
+ * @date: 2020/8/27
+ */
+public class FlyRocketPowered implements FlyBehavior{
+    public void fly() {
+        System.out.println("I can fly with a rocket!");
+    }
+}
